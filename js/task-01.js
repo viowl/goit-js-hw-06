b3deb7203cbceb62e44b2d21f741b89b5736e4fe
viowl <sovaworkbox@gmail.com>
2023-05-14 20:25:29 +0200

    1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categories = document.querySelectorAll(".item")
+const a = categories.length;
+console.log(a)
+
+const title = document.querySelectorAll("h2")
+console.log(title[0])
+
+const elements = document.querySelector("li")
+console.log(elements)
