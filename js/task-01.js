5f9cd17698a08ff4893f7b1c58b579d0928ca239
viowl <sovaworkbox@gmail.com>
2023-05-20 14:09:36 +0200

    Update task-01.js

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const categories = document.querySelectorAll(".item")
-const a = categories.length;
-console.log(a)
-
-const title = document.querySelectorAll("h2")
-console.log(title[0])
-
-const elements = document.querySelector("li")
-console.log(elements)
