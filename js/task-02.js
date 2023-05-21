df80bfa089f85f32c2d15665b27366ec573ab1a4
viowl <sovaworkbox@gmail.com>
2023-05-21 16:30:23 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const listItems = ingredients.map((ingredient) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  return listItem;
+});
+
+ingredientsList.append(...listItems);
+console.log(listItems)
+
+
